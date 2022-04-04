2de09cc7c44f74f7225c6790e372f0b28a268d3b
Alexiborra <alextraba88@gmail.com>
2022-04-04 20:12:02 +0200

    capadatos
    He añadido la conexion hacia la base de datos y he modificado el json

---
+++ Caroto/CapaDatos/CD_Conexion.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Microsoft.Extensions.Configuration.Json;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace CapaDatos
+{
+    public class CD_Conexion
+    {
+        private SqlConnection Conexion;
+        private bool Isconected = false;
+        public SqlConnection AbrirConexion()
+        {
+            if (!Isconected)
+            {
+                createConection();
+                Isconected = true;
+            }
+
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+
+        private void createConection()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var db = builder.Build().GetSection("ConnectionStrings").GetSection("BasdeDatos").Value;
+
+            Conexion = new SqlConnection(db);
+        }
+    }
+}
