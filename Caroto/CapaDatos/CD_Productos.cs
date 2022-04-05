be2f5b8291597dba246cc73c765732c86887e318
Alexiborra <alextraba88@gmail.com>
2022-04-05 20:45:00 +0200

    Conexion a BD
    he añadido las clases de capa datos y capa negocio

---
+++ Caroto/CapaDatos/CD_Productos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class CD_Productos
+    {
+        private CD_Conexion conexion = new CD_Conexion();
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+        public DataTable Mostrar()
+        {
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrarUsuarios";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+
+        }
+        public void Insertar(string correo, string contraseña)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsetarUsuarios";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@correo", correo);
+            comando.Parameters.AddWithValue("@contraseña", contraseña);
+
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+
+        }
+
+        public void Editar(string correo, string contraseña)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EditarUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@correo", correo);
+            comando.Parameters.AddWithValue("@contraseña", contraseña);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+        public void Eliminar(string correo)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@correo", correo);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+    }
+}
---
+++ Caroto/CapaNegocio/CN_Productos.cs
@@ -0,0 +1,31 @@
+﻿using CapaDatos;
+using System;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class CN_Productos
+    {
+        private CD_Productos objetoCD = new CD_Productos();
+        public DataTable MostrarUsu()
+        {
+            DataTable tabla = new DataTable();
+            tabla = objetoCD.Mostrar();
+            return tabla;
+        }
+        public void InsertarUsu(string correo, string contraseña)
+        {
+
+            objetoCD.Insertar(correo, contraseña);
+        }
+
+        public void EditarUsu(string correo, string contraseña)
+        {
+            objetoCD.Editar(correo, contraseña);
+        }
+        public void EliminarUsu(string correo)
+        {
+            objetoCD.Eliminar(correo);
+        }
+    }
+}
