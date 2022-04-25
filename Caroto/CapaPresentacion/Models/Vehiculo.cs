f38b8b7ca926f4722164863cc1b8243e3e6919c3
Alexiborra <alextraba88@gmail.com>
2022-04-25 19:06:58 +0200

    clase
    he creado una clase del vehiculo en la capa presentacion

---
+++ Caroto/CapaPresentacion/Models/Vehiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapaPresentacion.Models
+{
+    public class Vehiculo
+    {
+        public string coche { get; set; }
+        public string moto { get; set; }
+
+    }
+}
